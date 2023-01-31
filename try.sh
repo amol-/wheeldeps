2508459426e1d0a1d6f1226914c6e13cf482c201
Alessandro Molina <amol@turbogears.org>
2023-01-31 15:33:30 +0100

    Improve wheels shared objects consolidation

--- try.sh
+++ try.sh
@@ -6,6 +6,6 @@ pip install cython wheel auditwheel
 (cd libbar; make)
 (cd pylib1; ./makewheel.sh)
 (cd pylib2; ./makewheel.sh)
-pip install pylib1/wheelhouse/*.whl --force-reinstall
-pip install pylib2/wheelhouse/*.whl --force-reinstall
+python consolidatewheels.py pylib1/wheelhouse/*.whl pylib2/wheelhouse/*.whl
+pip install *.whl --force-reinstall
 python pyparent/test.py
